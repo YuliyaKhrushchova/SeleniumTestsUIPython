5f77da55a080c9a310b6019418feffd2d32a3380
Yuliya Khrushchova <hrustap@gmail.com>
2025-09-05 23:05:03 +0200

    fix_yml

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 @pytest.fixture(autouse=True)
 def browser():
     opts = webdriver.ChromeOptions()
+    opts.add_argument("--headless")
     opts.add_argument("start-maximized")
     browser = webdriver.Remote(
             command_executor='http://selenium:4444/wd/hub', options=opts)
