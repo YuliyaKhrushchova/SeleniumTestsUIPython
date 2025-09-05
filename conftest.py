248a99b1d29ff38f1563fcf152a30a70b427ccd8
Yuliya Khrushchova <hrustap@gmail.com>
2025-09-05 23:55:31 +0200

    fix_yml

--- conftest.py
+++ conftest.py
@@ -1,13 +1,16 @@
 import pytest
 from selenium import webdriver
-
+import os
+import time
 
 @pytest.fixture(autouse=True)
-def browser():
+def browser(request):
     opts = webdriver.ChromeOptions()
     opts.add_argument("--headless")
-    opts.add_argument("start-maximized")
-    browser = webdriver.Remote(
-            command_executor='http://localhost:4444', options=opts)
+    opts.add_argument("--window-size=1920,1080")
+    opts.add_argument("--start-maximized")
+    browser = webdriver.Chrome(options=opts)
+
+
     yield browser
     browser.quit()
