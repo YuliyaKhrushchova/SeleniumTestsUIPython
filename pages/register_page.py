c2d4a15c9b300683056c64e3f7a9097f20671b38
Yuliya Khrushchova <hrustap@gmail.com>
2025-09-08 22:25:43 +0200

    experiment

--- pages/register_page.py
+++ pages/register_page.py
@@ -1,14 +1,38 @@
 from .base_page import BasePage
-from .locators import RegisterPageLocators
+from .locators import RegisterPageLocators, BasePageLocators
+
 
 class RegisterPage(BasePage):
     def insert_email(self, email):
-        register_email_input = self.browser.find_element(*RegisterPageLocators.REGISTER_EMAIL_INPUT)
+        register_email_input = (
+            self.browser.find_element(
+                *RegisterPageLocators.REGISTER_EMAIL_INPUT))
         register_email_input.send_keys(email)
 
-    def insert_pass(self,password):
-        register_pass_input = self.browser.find_element(*RegisterPageLocators.REGISTER_PASS_INPUT)
+    def insert_pass(self, password):
+        register_pass_input = (
+            self.browser.find_element(
+                *RegisterPageLocators.REGISTER_PASS_INPUT))
         register_pass_input.send_keys(password)
 
-    def register(self, user_email, password):
-        register_email_input = self.browser.find_element(*RegisterPageLocators.REGISTER_EMAIL_INPUT)
+    def insert_confirm_pass(self, password):
+        register_confirm_pass_input = (
+            self.browser.find_element(
+                *RegisterPageLocators.REGISTER_PASS_CONFIRM_INPUT))
+        register_confirm_pass_input.send_keys(password)
+
+    def submit_registration(self):
+        registration_submit_btn = (
+            self.browser.find_element(
+                *RegisterPageLocators.REGISTER_SUBMIT_BTN))
+        registration_submit_btn.submit()
+
+    def register(self, user_email, password1, password2):
+        self.insert_email(user_email)
+        self.insert_pass(password1)
+        self.insert_pass(password2)
+        self.submit_registration()
+
+    def go_to_main_page(self):
+        main_page_link = self.browser.find_element(*BasePageLocators.LOGO)
+        main_page_link.click()
--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pages.login_page import LoginPage
 from utils.config import LOGIN_URL
 from utils.test_data import VALID_CREDS, INVALID_CREDS
@@ -5,34 +7,44 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as es
 
 
-def test_valid_login(browser):
-    page = LoginPage(browser,LOGIN_URL)
-    page.open()
-    page.insert_email(VALID_CREDS["email"])
-    page.insert_pass(VALID_CREDS["pass"])
-    page.submit_login()
-    WebDriverWait(browser, 10).until_not(es.url_to_be(LOGIN_URL))
-    browser.save_screenshot('./artifacts/login_valid.png')
+class TestLoginPage:
 
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_valid_login(self, browser):
+        page = LoginPage(browser, LOGIN_URL)
+        page.open()
+        page.insert_email(VALID_CREDS["email"])
+        page.insert_pass(VALID_CREDS["pass"])
+        page.submit_login()
+        WebDriverWait(browser, 10).until_not(es.url_to_be(LOGIN_URL))
+        # browser.save_screenshot('./artifacts/login_valid.png')
 
-def test_invalid_login(browser):
-    page = LoginPage(browser,LOGIN_URL)
-    page.open()
-    page.login(INVALID_CREDS["email"], INVALID_CREDS["pass"])
-    assert page.is_error_message_visible(), "Error message is not displayed"
-    assert browser.current_url == LOGIN_URL, f"User is redirected to {browser.current_url} "
-    browser.save_screenshot('./artifacts/login_invalid.png')
+    @pytest.mark.regression
+    def test_invalid_login(self, browser):
+        page = LoginPage(browser, LOGIN_URL)
+        page.open()
+        page.login(INVALID_CREDS["email"], INVALID_CREDS["pass"])
+        assert page.is_error_message_visible(), \
+            "Error message is not displayed"
+        assert browser.current_url == LOGIN_URL, \
+            f"User is redirected to {browser.current_url} "
+        # browser.save_screenshot('./artifacts/login_invalid.png')
 
-def test_go_to_register_page(browser):
-    page = LoginPage(browser, LOGIN_URL)
-    page.open()
-    page.go_to_register_page()
-    WebDriverWait(browser, 10).until_not(es.url_to_be(LOGIN_URL))
-    browser.save_screenshot('./artifacts/login_to_register.png')
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_go_to_register_page(self, browser):
+        page = LoginPage(browser, LOGIN_URL)
+        page.open()
+        page.go_to_register_page()
+        WebDriverWait(browser, 10).until_not(es.url_to_be(LOGIN_URL))
+        # browser.save_screenshot('./artifacts/login_to_register.png')
 
-def test_go_to_main_page(browser):
-    page = LoginPage(browser, LOGIN_URL)
-    page.open()
-    page.go_to_main_page()
-    WebDriverWait(browser, 10).until_not(es.url_to_be(LOGIN_URL))
-    browser.save_screenshot('./artifacts/login_to_main.png')
+    @pytest.mark.smoke
+    @pytest.mark.regression
+    def test_go_to_main_page(self, browser):
+        page = LoginPage(browser, LOGIN_URL)
+        page.open()
+        page.go_to_main_page()
+        WebDriverWait(browser, 10).until_not(es.url_to_be(LOGIN_URL))
+        # browser.save_screenshot('./artifacts/login_to_main.png')
